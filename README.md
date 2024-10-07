f91ad1258fdb1efa1c311b1791911421469e173f
FrancescoBenassi <francybenna77@gmail.com>
2024-10-08 00:48:12 +0200

    feat: fixed and added isNaN

--- README.md
+++ README.md
@@ -4,11 +4,10 @@ Il programma dovrà chiedere all'utente il numero di chilometri che vuole percor
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
 
 1) Chiedere il numero di km da percorrere
-2) Trasformare la stringa in numero
+2) Chiedere l'età del passeggero
 3) Validare che il numero non sia NaN
-4) Calcolare il prezzo del biglietto senza sconti (km * 0.21€)
-5) Chiedere l'età del passeggero
-6) Trasformare la stringa in numero
-7) Validare che il numero non sia NaN
-8) Applicare lo sconto in base all'età
-9) Se il passeggero è minorenne avrà uno sconto del 20% altrimenti se è over 65 avrà uno sconto del 40% altrimenti se non è minorenne e neanche over 65 non avrà nessun sconto
+4) Trasformare la stringa della distanza in numero
+5) Trasformare la stringa dell'età in numero
+6) Calcolare il prezzo del biglietto senza sconti (km * 0.21€)
+7) Applicare lo sconto in base all'età
+8) Se il passeggero è minorenne avrà uno sconto del 20% altrimenti se è over 65 avrà uno sconto del 40% altrimenti se non è minorenne e neanche over 65 non avrà nessun sconto
--- js/main.js
+++ js/main.js
@@ -9,35 +9,38 @@ const salesUnder = 20 / 100;
 //1) Chiedere il numero di km da percorrere
 
 const distance = prompt('Inserisci i km da percorre');
-console.log({ distance });
+console.log(distance);
 
-//2) Trasformare la stringa in numero
+//2) Chiedere l'età del passeggero
 
-const distanceNumber = parseInt(distance);
-console.log({ distanceNumber });
+const age = prompt('Inserisci la tua età');
+console.log(age);
 
 //3) Validare che il numero non sia NaN
 
+if (isNaN(distance) || isNaN(age)) {
+ alert('Errore');
+} else {
+ console.log('Sono stati inseriti numeri nelle aree richieste, si può procedere con il calcolo del prezzo');
+}
 
-//4) Calcolare il prezzo del biglietto senza sconti (km * 0.21€)
+//4) Trasformare la stringa della distanza in numero
 
-const priceFull = distanceNumber * price;
-console.log({ priceFull });
+const distanceNumber = parseInt(distance);
+console.log(distanceNumber);
 
-//5) Chiedere l'età del passeggero
-
-const age = prompt('Inserisci la tua età');
-console.log({ age });
-
-//6) Trasformare la stringa in numero
+//5) Trasformare la stringa dell'età in numero
 
 const ageNumber = parseInt(age);
-console.log({ ageNumber });
+console.log(ageNumber);
 
-//7) Validare che il numero non sia NaN
+//6) Calcolare il prezzo del biglietto senza sconti (km * 0.21€)
 
-/*8) Applicare lo sconto in base all'età
-  9) Se il passeggero è minorenne avrà uno sconto del 20% altrimenti 
+const priceFull = distanceNumber * price;
+console.log(priceFull);
+
+/*7) Applicare lo sconto in base all'età
+  8) Se il passeggero è minorenne avrà uno sconto del 20% altrimenti 
 se è over 65 avrà uno sconto del 40% altrimenti se non è minorenne e neanche over 65 non avrà nessun sconto */
 
 if (ageNumber > over) {
